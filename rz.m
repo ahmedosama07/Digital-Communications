96cb1df5e2689294d98d88103cb8599a941d0acc
Ahmed Osama <ahmed.osama8282@gmail.com>
2024-05-01 22:46:40 +0300

    Create rz.m

---
+++ rz.m
@@ -0,0 +1,17 @@
+function [waveform, t] = rz(bits, bitrate)
+%rz Summary of this function goes here
+%   Function generates waveform for polar return to zero line coding
+%   technique.
+Fs = 100;
+T = length(bits)/bitrate;
+t = linspace(0, T, Fs*length(bits));
+waveform = zeros(1, length(t));
+for i = 0: length(bits)- 1
+    if bits(i+1) == 1
+        waveform((i*Fs)+1 : (i+0.5)*Fs) = 1;
+    else
+        waveform((i*Fs)+1 : (i+0.5)*Fs) = -1;
+    end
+end
+end
+
